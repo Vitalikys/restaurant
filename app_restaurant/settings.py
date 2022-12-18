13cb56e6aa3d2ba5496f28b8474693169f92eeef
vitalikys <vitalikys87@gmail.com>
2022-12-19 01:20:12 +0200

    edit settings.py

--- app_restaurant/settings.py
+++ app_restaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hx&55a$^oi4&7lguctih5d)ot=93*8166h@+=_zj$p949-hp^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '78.27.202.55']
 AUTH_USER_MODEL = 'restaurant_user.RestaurantUser'
 
 # Application definition
